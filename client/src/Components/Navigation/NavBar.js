445a91409862720c98d4de7c2634349a425206b3
Gongo <jvilla1714@gmail.com>
2020-02-11 19:32:16 -0500

    Added component for nav bar

---
+++ client/src/Components/Navigation/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../../Styles/NavBar.scss'
+
+export default function NavBar() {
+    return (
+        <div className = "Nav">
+            <nav>
+            </nav>
+        </div>
+    )
+}
