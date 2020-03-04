7929160995068a089f8f06877370e27c4fa1083d
Gongo <jvilla1714@gmail.com>
2020-03-03 20:14:28 -0500

    Added the structure for the site

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom'
+import ProjectsPage from './Components/Projects/ProjectsPage'
+import ContactPage from './Components/Contact/ContactPage'
+import AboutPage from './Components/About/AboutPage'
+import HomePage from './Components/Home/HomePage'
 import NavBar from './Components/Navigation/NavBar'
 
 function App() {
@@ -6,7 +11,12 @@ function App() {
       <>
         <NavBar/>
         <main>
-
+          <Switch>
+            <Route path = "" component = {HomePage} exact />
+            <Route path = "/About" component = {AboutPage} />
+            <Route path = "/Projects" component = {ProjectsPage} />
+            <Route path = "/Contact" component = {ContactPage} />
+          </Switch>
         </main>
       </>
   );
