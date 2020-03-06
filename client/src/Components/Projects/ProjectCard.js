73f6af59e30340391831ab1b013e31082a1a273f
Gongo <jvilla1714@gmail.com>
2020-03-05 21:56:12 -0500

    aligned images on the project

--- client/src/Components/Projects/ProjectCard.js
+++ client/src/Components/Projects/ProjectCard.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import '../../Styles/ProjectCard.scss'
+import TestImage from '../Images/test image.jpg'
 
 export default function ProjectCard() {
     return (
         <div className = "Card-bg">
             <div className = "Card-Header">
-
+                <img src={TestImage} alt=""/>
             </div>
             <div className="Card-Body">
                 
