4c2af27864bb49f92ddb8783611abf62cafbc72d
Gongo <jvilla1714@gmail.com>
2020-03-09 19:48:39 -0500

    images stack on each other

--- client/src/Components/Projects/ProjectCard.js
+++ client/src/Components/Projects/ProjectCard.js
@@ -5,18 +5,9 @@ import TestImage from '../Images/test image.jpg'
 export default function ProjectCard() {
     return (
         <div className = "Card-bg">
-            <div className = "Card-Header">
-                <img src={TestImage} alt=""/>
-            </div>
-            <div className="Card-Body">
-                
-            </div>
-            <div className="Card-Skills">
-
-            </div>
-            <div className="Card-Footer">
-
-            </div>
+            <img src={TestImage} alt="" />
+                <div className = "Card-Header">
+                </div>
         </div>
     )
 }
