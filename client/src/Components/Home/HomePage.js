166def57cd3c759acc444f7fbb2c24c1696c0f7a
Gongo <jvilla1714@gmail.com>
2020-03-17 19:46:23 -0500

    Added message for home page

--- client/src/Components/Home/HomePage.js
+++ client/src/Components/Home/HomePage.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
+import '../../Styles/HomePage.scss'
 
 export default function HomePage() {
     return (
-        <div>
-            Home
+        <div className="Home-Flex">
+            <span>
+                Hello, my name is Jorge Villarreal
+            </span>
+            <NavLink to="/Projects">My Work</NavLink>
         </div>
     )
 }
