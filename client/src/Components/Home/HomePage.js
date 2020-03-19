be1ae807ec928afb7876f2f1c8a109baa30b752a
Gongo <jvilla1714@gmail.com>
2020-03-18 19:58:18 -0500

    Added animated background to home page

--- client/src/Components/Home/HomePage.js
+++ client/src/Components/Home/HomePage.js
@@ -4,11 +4,18 @@ import '../../Styles/HomePage.scss'
 
 export default function HomePage() {
     return (
+        <>
+            <div class="background-container">
+                    <div class="stars"></div>
+                    <div class="twinkling"></div>
+                    <div class="clouds"></div>
+            </div>
         <div className="Home-Flex">
             <span>
                 Hello, my name is Jorge Villarreal
             </span>
-            <NavLink to="/Projects">My Work</NavLink>
+            <NavLink className = "Project-Re" to="/Projects">My Work</NavLink>
         </div>
+        </>
     )
 }
