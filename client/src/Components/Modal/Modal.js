ef43fe5a456a17326cf1abe6ebca854c1ea7a0a7
Gongo <jvilla1714@gmail.com>
2020-03-23 19:17:49 -0500

    Added component for technologies

--- client/src/Components/Modal/Modal.js
+++ client/src/Components/Modal/Modal.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Technologies from './Technologies'
+
 import '../../Styles/Modal.scss'
 
 export default function Modal({show, toggleShow}) {
@@ -7,11 +9,31 @@ export default function Modal({show, toggleShow}) {
             <>
             <div className = "Modal-bg"/>
                 <div className="Modal-Content">
-                    <span onClick={() => toggleShow(false)}>
-                        <i className="fas fa-times">
+                    <section className = "Modal-Header">
+                        <span onClick={() => toggleShow(false)}>
+                            <i className="fas fa-times">
                             </i>
-                    </span>
-
+                        </span>
+                    </section>
+                    <section className = "Modal-Body">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis
+                             lectus ornare, viverra lorem luctus, viverra metus. Donec mattis
+                              at nisi non posuere. Cras dictum magna mauris, at ultricies arcu finibus 
+                              ac. Maecenas eu metus eget lorem malesuada molestie. Sed vel risus in 
+                              justo hendrerit aliquam. Nulla sed justo gravida, luctus enim at, commodo 
+                              sem. Donec diam dui, tristique vitae dapibus in, eleifend vitae nisl. 
+                              Aliquam convallis leo ac dapibus gravida. Etiam malesuada, ante a aliquam
+                               laoreet, libero ex condimentum arcu, imperdiet dignissim magna ante et lectus.
+                                Fusce suscipit, ipsum nec consectetur feugiat, elit arcu placerat nulla,
+                                 a maximus eros diam at nunc. Nunc in magna vitae neque facilisis fermentum
+                                  ac ac lorem. Quisque suscipit magna eu velit scelerisque, quis gravida 
+                                  odio placerat. Suspendisse potenti. 
+                        </p>
+                        <div>
+                            <Technologies/>
+                        </div>
+                    </section>
                 </div>
             </>
         )
---
+++ client/src/Components/Modal/Technologies.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../../Styles/Technologies.scss'
+
+export default function Technologies() {
+    return (
+        <div className="Tech">
+            Zawarudo
+        </div>
+    )
+}
