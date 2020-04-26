81679ccf1c1e54a6f9ce16cdf645561a8b275e99
Gongo <jvilla1714@gmail.com>
2020-04-26 00:17:16 -0500

    Cleaned up code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom'
-import ProjectsPage from './Components/Projects/ProjectsPage'
-import ContactPage from './Components/Contact/ContactPage'
-import AboutPage from './Components/About/AboutPage'
-import HomePage from './Components/Home/HomePage'
-import NavBar from './Components/Navigation/NavBar'
+import {
+    ProjectsPage, 
+    ContactPage, 
+    AboutPage, 
+    HomePage, 
+    NavBar
+    } from './Components'
 
-function App() {
+const App: React.FC = () => {
   return (
       <>
         <NavBar/>
---
+++ client/src/Components/index.tsx
@@ -0,0 +1,6 @@
+export {default as AboutPage} from './About/AboutPage';
+export {default as ContactPage} from './Contact/ContactPage';
+export {default as HomePage} from './Home/HomePage'
+export {default as NavBar} from './Navigation/NavBar'
+export {default as ProjectsPage} from './Projects/ProjectsPage'
+
